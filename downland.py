33ce38f193d2a717e5ef4ce9876653908fb481c6
robert <saakyan-robert@rambler.ru>
2022-05-17 23:42:40 +0300

    корректное создание директорий

--- downland.py
+++ downland.py
@@ -106,6 +106,8 @@ def downland(save_directory, start_date, next_date, ):
     for meeting in tqdm(response_data['meetings']):
         TOTAL_FILES += len(meeting['recording_files'])
         if not os.path.isdir(dir := '{}_{}'.format(meeting['topic'], meeting['start_time'])):
+            if '/' in dir:
+                dir = dir.replace('/', '')
             os.mkdir(dir)
         for recording_file in meeting['recording_files']:
             downland_recording(recording_file['download_url'],
